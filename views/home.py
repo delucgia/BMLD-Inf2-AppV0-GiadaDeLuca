d944278b55c3f8017356e5b2d17f14f92b2ac0f7
delucgia <delucgia@students.zhaw.ch>
2026-02-25 11:28:07 +0100

    update

--- views/home.py
+++ views/home.py
@@ -2,14 +2,12 @@ import streamlit as st
 
 st.title("Meine erste Streamlit App")
 
-# !! WICHTIG: Eure Emails müssen in der App erscheinen!!
-
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Giada De Luca (delucgia@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Giada De Luca (delucgia@studentszhaw.ch)
+- Priscilla Baccaro (baccapri@students.zhaw.ch)
+- Ana Lucia Gomes Carvalho (gomesana@students.zhaw.ch)
 
-Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
-
-Autor: Giada De Luca, 
+Diese App ist dient 
+Autor: Giada De Luca, Priscilla Baccaro, Ana Lucia Gomes Carvalho
 """
